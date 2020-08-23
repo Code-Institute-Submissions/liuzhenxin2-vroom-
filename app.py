f9032965ef0322e86f67ed10f73f7510bfbf76d2
liuzhenxin2 <liuzhenxin1@live.com>
2020-08-23 08:12:19 +0000

    added login system and checking if email exists

--- app.py
+++ app.py
@@ -37,10 +37,13 @@ def user_loader(email):
 
     # if the email exists
     if user:
-        # create a User object that represents the user
+        # create a User object that represents the user session
         user_object = User()
+        # store the email of the user in the session as `id`
         user_object.id = user["email"]
-         # return the User object
+        # store the unique id of the user in the session as `account_id`
+        user_object.account_id = user["_id"]
+        # return the User object
         return user_object
     else:
         # if the email does not exist in the database, report an error
@@ -62,19 +65,28 @@ def process_register():
     # extract out the email and password
     email = request.form.get('email')
     password = request.form.get('password')
+    username = request.form.get('username')
+    phone = request.form.get('phone')
 
     # TODO: Vadliate if the email and password are proper
 
-    # Create the new user
-    db.users.insert_one({
-        'email': email,
-        'password': pbkdf2_sha256.hash(password)
-    })
+    if email:
+        flash("Email already exists", "danger")
+        return redirect(url_for('register'))
+    
+    else:
+        # Create the new user
+        db.users.insert_one({
+            'email': email,
+            'password': pbkdf2_sha256.hash(password),
+            'username': username,
+            'phone': phone
+        })
 
-    flash("Sign up successful", "success")
+        flash("Sign up successful", "success")
 
-    # Redirect to the login page
-    return redirect(url_for('login'))
+        # Redirect to the login page
+        return redirect(url_for('login'))
 
 
 @app.route('/login')
@@ -97,18 +109,29 @@ def process_login():
     if user and pbkdf2_sha256.verify(password, user["password"]):
         # if the password matches, authorize the user
         user_object = User()
+        # save the email of the user in the session as the property `id`
         user_object.id = user["email"]
+        # save the unique id of the user in the session as the property
+        # `account_id`
+        user_object.account_id = user["_id"]
+        # create the user session
         flask_login.login_user(user_object)
 
         # redirect to a page and says login is successful
         flash("Login successful", "success")
-        return redirect(url_for('home'))
+        return redirect(url_for('show_all_listings'))
 
     # if the login failed, return back to the login page
     else:
         flash("Wrong email or password", "danger")
         return redirect(url_for('login'))
 
+@app.route('/profile')
+@flask_login.login_required
+def profile():
+    email = flask_login.current_user.id
+    account_id = flask_login.current_user.account_id
+    return f"Email = {email}, account_id={account_id}"
 
 @app.route('/logout')
 def logout():
